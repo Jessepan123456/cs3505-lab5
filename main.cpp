4e99010972a76efd740cfd5222061bf1d1c34a70
Jesse <Jessepan123456@gmail.com>
2026-09-23 11:22:11 -0600

    Merged

--- main.cpp
+++ main.cpp
@@ -3,11 +3,7 @@
 int main()
 {
     std::cout << "Hello from Git!\n";
-<<<<<<< HEAD
     int i = 7;
-=======
-    int i = 10;
->>>>>>> 65ec036a2721293fe8f470d84e41a5e3d8e00013
     int y = 2;
     int j = 10;
     return 0;
